7995210d3552ea915d6e7b29e3f6087c0f2c33fb
Arnold Schelb <ardrift@gmail.com>
2022-03-16 01:20:19 +0100

    Add all original files

---
+++ AdasVetelClient/AdasVetelClient/forms/ConnectForm.cs
@@ -0,0 +1,45 @@
+﻿using AdasVetelClient.tcp;
+using AdasVetelServer.messages;
+using System;
+using System.Windows.Forms;
+
+
+namespace AdasVetelClient
+{
+    public partial class ConnectForm : MaterialSkin.Controls.MaterialForm, ClientView
+    {
+        public ConnectForm()
+        {
+            InitializeComponent();
+        }
+        public void connected()
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        public void disconnected()
+        {
+
+        }
+        public void clientLoggedIn() { }
+
+        public void handleMessage(MessageBase message, string type)
+        {
+        }
+
+        public bool isActive()
+        {
+            return !IsDisposed;
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void ConnectForm_Load(object sender, EventArgs e)
+        {
+            TcpClientWrapper.Instance.views.Add(this);
+        }
+    }
+}
